5df50fa73776545b5383a498510a4692d3a1335d
David Augusto <daviaugusto222@hotmail.com>
2018-10-26 12:38:44 -0300

    Aula 3

--- app/infra/ProdutosDAO.js
+++ app/infra/ProdutosDAO.js
@@ -1,10 +1,13 @@
-class ProdutosDAO {
-    constructor(connection) {
-        this._connection = connection;
-    }
-    lista(callback) {
-        this._connection.query('select * from livros', callback);
-    }
+function ProdutosDAO(connection){
+    this._connection = connection;
+}
+
+ProdutosDAO.prototype.lista = function(callback){
+    this._connection.query('select * from livros',callback);
+}
+
+ProdutosDAO.prototype.salva = function(produto,callback){
+    this._connection.query('insert into livros set ?',produto,callback);
 }
 
 module.exports = function(){
--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,5 +1,6 @@
 module.exports = function(app){
-    app.get('/produtos',function(req,res){
+
+    var listaProdutos = function(req,res){
         var connection = app.infra.connectionFactory();
         var produtosDAO = new app.infra.ProdutosDAO(connection);
         produtosDAO.lista(function(err,results){
@@ -7,6 +8,25 @@ module.exports = function(app){
         });
 
         connection.end();
+    }
+
+    app.get('/produtos',listaProdutos);
+
+    app.get('/produtos/form',function(req,res){
+        res.render('produtos/form');
+    });
+
+    app.post('/produtos',function(req,res){
+        var produto = req.body;
+        var connection = app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+        produtosDAO.salva(produto,function(err,results){
+            res.redirect('/produtos');
+        });
+
+        connection.end();
         
     });
+
+   
 }
